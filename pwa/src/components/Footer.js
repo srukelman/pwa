01decd0b82b5b55656d693c9f027b758c76d57e0
Sean Kelman <seanrkelman@gmail.com>
2023-09-06 15:28:33 -0700

    removed instagram from footer cuz i wasnt using it anyway

--- pwa/src/components/Footer.js
+++ pwa/src/components/Footer.js
@@ -14,7 +14,6 @@ function Footer() {
                 <Button buttonStyle='btn--outline' buttonSize="btn--medium" path="https://github.com/srukelman" icon="fa fa-github"/>
                 <Button buttonStyle='btn--outline' buttonSize="btn--medium" path="mailto:seanrkelman@gmail.com" icon="fa fa-envelope-o"/>
                 <Button buttonStyle='btn--outline' buttonSize="btn--medium"path="https://www.linkedin.com/in/sean-kelman-633057211/" icon="fa fa-linkedin-square"/>
-                <Button buttonStyle='btn--outline' buttonSize="btn--medium" path="https://github.com/srukelman" icon="fa fa-instagram"/>
             </div> 
         </section>
     </div>
