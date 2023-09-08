c7c1c991492e792409e9a43deab646ad574bf533
Sean Kelman <seanrkelman@gmail.com>
2023-09-08 10:41:27 -0700

    created post request to get current playing song on spotify...not quite working

--- pwa/src/components/pages/About.js
+++ pwa/src/components/pages/About.js
@@ -2,11 +2,13 @@ import React from 'react'
 import '../../App.css'
 import Footer from '../Footer'
 import Sections from '../Sections'
+import { PostRequest } from '../PostRequest'
 
 function About() {
   return (
     <>
     <Sections />
+    <PostRequest />
     <Footer />
     </>
     
